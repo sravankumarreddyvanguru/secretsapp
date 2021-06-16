7790284f35bc3e89cde31e08e261cac3630bdb13
sravankumarreddy <sravankumarreddyvanguru@gmail.com>
2021-06-16 18:12:52 +0530

    last commit

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID:process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL:"https://serene-inlet-23758.herokuapp.com/secrets",
+    callbackURL:"https://serene-inlet-23758.herokuapp.com/auth/google/secrets",
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
